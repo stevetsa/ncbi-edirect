e09acd673fbfde804de7683af2387a4f5d88bafd
Steve Tsang <mylagimail2004@yahoo.com>
2025-09-26 14:19:39 -0400

    Update taxonomy.sh

--- taxonomy.sh
+++ taxonomy.sh
@@ -4,5 +4,5 @@ while read org
   do
     esearch -db taxonomy -query "$org [LNGE] AND family [RANK]" < /dev/ null |
     efetch -format docsum |
-    xtract -pattern Document Summary -lbl "$org" -element ScientificName Division
+    xtract -pattern DocumentSummary -lbl "$org" -element ScientificName Division
 done
